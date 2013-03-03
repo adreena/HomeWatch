e82ceb1f2d42980e12dac5a40ee57b0dcb5c0584
Eddie Antonio Santos <easantos@ualberta.ca>
2013-03-02 17:00:19 -0700

    Reorganized templates.

--- www/crazy_eddie_stuff/achievements.php
+++ www/crazy_eddie_stuff/achievements.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 
 /* Setup Twig environment. */
-$twig = require(__DIR__ . '/twig_config.inc');
+$twig = TwigSingleton::getInstance();
 
 /* Initialize all of these! */
 $model = new \UASmartHome\Model();
@@ -13,6 +13,6 @@ $view = new \UASmartHome\View($controller, $model);
 
 $achievements = $view->getAchievements();
 
-echo $twig->render('resident-achievements.html',
+echo $twig->render('resident/achievements.html',
     array('achievements' => $achievements));
 
--- www/crazy_eddie_stuff/scoreboard.php
+++ www/crazy_eddie_stuff/scoreboard.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /* Setup Twig environment. */
-$twig = require(__DIR__ . '/twig_config.inc');
+$twig = TwigSingleton::getInstance();
 
 /* This one is "dynamic"; it doesn't need any data to be produced whilst 
  * rendering the template. */
 
-echo $twig->render('resident-scoreboard.html');
+echo $twig->render('resident/scoreboard.html');
 
