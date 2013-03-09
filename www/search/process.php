93562a3bced7991e693773664cd73b9314348b8c
Devin Hanchar <hanchar@cs.ualberta.ca>
2013-03-09 14:28:35 -0700

    Small update to process

--- www/search/process.php
+++ www/search/process.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-require_once __DIR__ . '/../vendor/autoload.php';
+//require_once __DIR__ . '/../vendor/autoload.php';
 
 $sensors = array();
 $apartments = array();
@@ -85,42 +85,6 @@ if(ISSET($startdate && $enddate) {
 		$jsonResults += ', "date": "'+$date+'",';
 	}
 	$jsonResults += '"granularity" : "'+$period+'"';
-	echo $jsonResults;
+	echo json_encode($jsonResults);
 }
-/*
-//the following code chunk needs to be replaced by result set
-$data_file = file_get_contents("test.json");
-$jsonIterator = new RecursiveIteratorIterator(
-  new RecursiveArrayIterator(json_decode($data_file, TRUE)),
-  RecursiveIteratorIterator::SELF_FIRST);
-
-
-$index = 0;
-
-foreach($jsonIterator as $key => $value) {
-  if(is_array($value)) {
-    // do nothing
-  } else {
-    if($key == "Date"){
-      $temp_date = $value;
-      continue;
-    } else {
-      if($temp_date == $date) {
-        if($key == "Apt") {
-          $temp_apart = $value;
-	  continue;
-        } else {          
-	  if($temp_apart == "2") {
-            if($key == "CO2") {
-              $carbon_dioxide[$index] = $value;
-              $index++;
-            }
-          }
-        }        
-      }
-    }
-  }
-}
-echo json_encode($carbon_dioxide);
-*/
 
