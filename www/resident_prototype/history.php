5b1e5c087f71d6ea8a65e558d930600f687976b7
seth <seth@beamdog.com>
2013-03-09 12:24:10 -0700

    Update to resident pages. Adding advice page and setup for history page

---
+++ www/resident_prototype/history.php
@@ -0,0 +1,19 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<title>SmartHome - Score Board</title>
+</head>
+
+<body>
+
+    <div id="container">
+
+	<?php require_once 'sidebar.php'?>
+
+	<div id="content">
+	</div>
+</body>
+
+</html>
