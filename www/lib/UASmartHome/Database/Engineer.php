c780ca768c149de5b0015f34a1d66cea96ae73bd
tsungyong <tsunglin@ualberta.ca>
2013-03-15 14:05:26 -0600

    Updated Devin's stuff
    Database/Engineer.php stuff was changed
    process.php was changed

--- www/lib/UASmartHome/Database/Engineer.php
+++ www/lib/UASmartHome/Database/Engineer.php
@@ -1,50 +1,116 @@
 <?php
 
 include 'config.php';
+//require_once __DIR__ . '/../vendor/autoload.php';
+
+
+
+
+//$tables = array("Relative Humidity"=>"Air", "Temperature" => "Air", "CO2"=>"Air");
+
+
 
 class Engineer {
 
 //Engineer
 
+//$tables = array("Relative Humidity"=>"Air", "Temperature" => "Air", "CO2"=>"Air"); 
 
 
-	function db_pull_query($apt, $table, $date, $period) {
-		$dateparts = explode($date, "-");
+
+
+	function db_pull_query($apt, $column, $startdate, $enddate, $period) {
+		
+
+		$tables = array("RelHum"=>"Air", "Temperature" => "Air", "CO2"=>"Air", "HotWater"=>"Water", "TotalWater"=>"Water", "Insulation"=>"Heat", "Stud"=>"Heat", "CurrentFlow"=>"Heat", "CurrentTemp"=>"Heat", "TotalMass"=>"Heat", "TotalEnergy"=>"Heat", "TotalVol"=>"Heat");
+
+		$dateparts = explode("-", $startdate);
 		$year = $dateparts[0];
 		$month = $dateparts[1];
 		$day = $dateparts[2];
 
-		if ($period == "Hourly") {
+		$table = $tables[$column];
+
+		$results = array();
+		//echo $startdate."\n".$enddate;
+		$startdate = date_create_from_Format('Y-m-d', $startdate);
+		$enddate = date_create_from_Format('Y-m-d', $enddate);
+		//$loopkill = false;
+	//	echo $startdate."\n".$enddate;
+		//while (!$loopkill) { 
+		//:	echo "5";
+			//echo var_dump($startdate);
+			//echo var_dump($enddate);
+			if ($period == "Hourly") {
+				while ($startdate <= $enddate) {
+					$temp = Engineer::db_query_Hourly($apt,$table,$startdate->format('Y-m-d'), $column);
+					$results[$startdate->format('Y-m-d')] = $temp[0];
+					$startdate->add(date_interval_create_from_date_string('1 hour'));
+				}	
 			//return db_query_Hourly(
 			//Needs the "get every hour" function
-		} else if ($period == "Daily") {
-			return db_query_Daily($apt, $table, $date);
-		} else if ($period == "Weekly") {
-			$week = date_create_from_format("d-M-Y", $date);
-			//return db_query_Weekly($apt, $table, $
-			//CANT GET WEEK NUMBER RELIABLY
-		} else if ($period == "Monthly") {
-			return db_query_Monthly($apt, $table, $year, $month); 
-		} else if ($period == "Yearly") {
-			return db_query_Yearly($apt, $table, $year);
-		}
-		
+			} else if ($period == "Daily") {
+				//$results[$startdate] = Engineer::db_query_Daily($apt, $table, $startdate->format('Y-m-d'), $column)[0][$column];
+				while ($startdate <= $enddate) {
+					$temp = Engineer::db_query_Daily($apt, $table, $startdate->format('Y-m-d'), $column); 
+					$results[$startdate->format('Y-m-d')] = $temp[0]; 
+					$startdate->add(date_interval_create_from_date_string('1 day'));
+			}
+				//echo var_dump($startdate);
+				//$startdate
+			} else if ($period == "Weekly") {
+				$week = date_create_from_format("d-M-Y", $date);
+				//return db_query_Weekly($apt, $table, $
+				//CANT GET WEEK NUMBER RELIABLY
+			} else if ($period == "Monthly") {
+				$endyear = $enddate->format("Y");
+				$endmonth = $enddate->format("n");
+				$year = $startdate->format("Y");
+				$month = $startdate->format("n");
+				while ($month <= $endmonth && $year <= $endyear) {
+					$temp = Engineer::db_query_Monthly($apt, $table, $year, $month, $column);
+					$results[$startdate->format('Y-m-d')] =  $temp[0];
+					$startdate->add(date_interval_create_from_date_string('1 month')); //TODO: This requires a new method that adds days based on the month because this PHP just adds 30 day
+
+			        	$year = $startdate->format("Y");
+	                                $month = $startdate->format("n");
+
+				}
+			} else if ($period == "Yearly") {
+				$endyear = $enddate->format("Y");
+				$year = $startdate->format("Y");
+				while ($year <= $endyear) {
+
+					$temp = Engineer::db_query_Yearly($apt, $table, $year, $column);
+					$results[$startdate->format('Y-m-d')] = $temp[0];
+					$startdate->add(date_interval_create_from_date_string('1 year'));
+					$year = $startdate->format("Y");
+
+					//return db_query_Yearly($apt, $table, $year, $column)[0][$column];
+				}
+			}
+		//}
+	
+
+
+		return $results;
+	
 	}
 
 
 
-	function db_query_Yearly($apt,$table,$Year)
+	function db_query_Yearly($apt,$table,$Year,$column)
 	{
 	           
 			   
 			    $result =array();
 			    $table .= '_Yearly';
-		        $sql=$GLOBALS['conn']->prepare("select * from ".$table." where Apt= :Apt_Num AND Year= :Year ") ;
+		        $sql=$GLOBALS['conn']->prepare("select ".$column." from ".$table." where Apt= :Apt_Num AND Year= :Year ") ;
 				$sql->bindValue(":Apt_Num",$apt);
 				$sql->bindValue(":Year",$Year);
 				$sql->execute();
 				$row_count= $sql->rowCount();
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 				$result[]=(array)$row;
 				}
@@ -52,71 +118,71 @@ class Engineer {
 				return $result;
 	}
 	
-function db_query_Monthly($apt,$table,$Year,$Month)
+function db_query_Monthly($apt,$table,$Year,$Month,$column)
 	{
 			    $result =array();
 			    $table .= '_Monthly';
-		        $sql=$GLOBALS['conn']->prepare("select * from ".$table." where Apt= :Apt_Num AND Year= :Year AND Month= :Month ") ;
+		        $sql=$GLOBALS['conn']->prepare("select ".$column." from ".$table." where Apt= :Apt_Num AND Year= :Year AND Month= :Month ") ;
 				$sql->bindValue(":Apt_Num",$apt);
 				$sql->bindValue(":Year",$Year);
 				$sql->bindValue(":Month",$Month);
 				$sql->execute();
 				$row_count= $sql->rowCount();
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 				$result[]=(array)$row;
 				}
 				$a= $sql->rowCount();
 				return $result;
 	}
-	function db_query_Weekly($apt,$table,$Year,$Week)
+	function db_query_Weekly($apt,$table,$Year,$Week,$column)
 	{
 			    $result =array();
 			    $table .= '_Weekly';
-		        $sql=$GLOBALS['conn']->prepare("select * from ".$table." where Apt= :Apt_Num AND Year= :Year AND Week= :Week") ;
+		        $sql=$GLOBALS['conn']->prepare("select ".$column." from ".$table." where Apt= :Apt_Num AND Year= :Year AND Week= :Week") ;
 				$sql->bindValue(":Apt_Num",$apt);
 				$sql->bindValue(":Year",$Year);
 				$sql->bindValue(":Week",$Week);
 				$sql->execute();
 				$row_count= $sql->rowCount();
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 				$result[]=(array)$row;
 				}
 				$a= $sql->rowCount();
 				return $result;
 	}
-	function db_query_Daily($apt,$table,$date)
+	function db_query_Daily($apt,$table,$date,$column)
 	{
 	           
 			   
-			    $result =array();
-			    $table .= '_Daily';
-		        $sql=$GLOBALS['conn']->prepare("select * from ".$table." where Apt= :Apt_Num AND Date= :Date ") ;
+				$result =array();
+				$table .= '_Daily';
+			        $sql=$GLOBALS['conn']->prepare("select ".$column.", Date from ".$table." where Apt= :Apt_Num AND Date= :Date ") ;
 				$sql->bindValue(":Apt_Num",$apt);
 				$sql->bindValue(":Date",$date);
 				$sql->execute();
 				$row_count= $sql->rowCount();
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 				$result[]=(array)$row;
 				}
 				$a= $sql->rowCount();
 				return $result;
 	}
-	function db_query_Hourly($apt,$table,$date,$Hour)
+	function db_query_Hourly($apt,$table,$date,$Hour,$column)
 	{
 	           
 			   
 			    $result =array();
 			    $table .= '_Hourly';
-		        $sql=$GLOBALS['conn']->prepare("select * from ".$table." where Apt= :Apt_Num AND Date= :Date AND Hour= :Hour ") ;
+		        $sql=$GLOBALS['conn']->prepare("select ".$column." from ".$table." where Apt= :Apt_Num AND Date= :Date AND Hour= :Hour ") ;
 				$sql->bindValue(":Apt_Num",$apt);
 				$sql->bindValue(":Date",$date);
 				$sql->bindValue(":Hour",$Hour);
 				$sql->execute();
 				$row_count= $sql->rowCount();
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 				$result[]=(array)$row;
 				}
@@ -177,7 +243,7 @@ function db_query_Monthly($apt,$table,$Year,$Month)
 				array_push($result,$errinf[0]);
 				array_push($result,$errinf[1]);
 				array_push($result,$errinf[2]);
-				while ($row = $sql->fetch(PDO::FETCH_OBJ))
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC))
 				{
 					array_push($result,$row);
 				}
@@ -190,6 +256,8 @@ function db_query_Monthly($apt,$table,$Year,$Month)
 
 // Example code
 // must code
+//This should all be  in a test method, because this file has tobe included elsewhere
+/*
 $testdb=new Engineer ();
 
 //Test it Yearly
@@ -241,5 +309,5 @@ print_r($r4);
 echo "<br>";
 echo "===========================";
 echo "<br>";
-
+*/
 ?>
--- www/search/process.php
+++ www/search/process.php
@@ -5,14 +5,66 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 /* We want to use the ENGINEER database access class as just 'Engineer'. */
-use \UASmartHome\Database\Engineer;
+//use www\lib\UASmartHome\Database\Engineer;
+
+require_once __DIR__. '/../lib/UASmartHome/Database/Engineer.php';
 
 /* Aw yea, baby. We just gon' spit out sum JSON. */
 header('Content-Type: application/json; charset=utf-8');
 
+
+//echo json_encode($_GET);
+//exit();
+
+
+$test = true;
+
+//echo var_dump($_GET);
+
+
 $sensors = array();
 $apartments = array();
-$carbon_dioxide = array();
+$message = "";
+//$period = "";
+//$startdate = "";
+//$enddate = "";
+
+if (ISSET($_GET['sensors'])) {
+	$sensors = $_GET['sensors'];
+} else {
+	$message .= "No sensors selected.\n";
+}
+
+if (ISSET($_GET['apartments'])) {
+	$apartments = $_GET['apartments'];
+} else {
+	$message .= "No apartments selected\n";
+}
+
+if (ISSET($_GET['startdate'])) {
+	$startdate = $_GET['startdate'];
+} else {
+	$message .= "No start date selected \n";
+}
+
+if (ISSET($_GET['enddate'])) {
+	$enddate = $_GET['enddate'];
+} else {
+	$message .= "No end date selected\n";
+}
+
+if (ISSET($_GET['period'])) {
+	$period = $_GET['period'];
+} else {
+	$message .= "No granularity selected\n";
+}
+
+//echo $message;
+
+if ($test == false && $message > "" ) {
+	echo json_encode(array("message"=>$message));
+	exit();
+}
 
 $SECONDS_PER_HOUR = 60*60;
 $HOURS_PER_DAY = 24;
@@ -23,81 +75,85 @@ $DAILY_VIEW_MAX = 14;
 $WEEKLY_VIEW_MAX = 12;
 $MONTHLY_VIEW_MAX = 12;
 
-$date = true; //testing
-$startdate = null; //testing
-$enddate = null; //testing
+//$onedate = false;
+//$date = true; //testing
+//$startdate = null; //testing
+//$enddate = null; //testing
 
 //$period = $_GET["period"]; //Granularity of data
 //$apartments = $_GET['apartments'];
 //$sensors = $_GET['sensors'];
 
-$apartments = array("apt1, apt2");
-$sensors = array("air");
-$period = "daily";
 
+
+if ($test) {
+	$apartments = array(1, 2);
+	$sensors = array("Temperature", "CO2");
+//$data = array("Temperature", "CO2");
+	$period = "Daily";
+	$message = "Success!";
+	$startdate = "2012-02-29";
+	$enddate = "2012-03-01";
+}
 
 //if(ISSET($_GET['startdate'] && $_GET['enddate']) {
-if(ISSET($startdate) && ISSET($enddate)) {
+//if(!$onedate) {
 	//$startdate = $_GET["startdate"];
-	$startdate = "2012-01-01";
+	//$startdate = "2012-02-29";
 	//$enddate = $_GET['enddate'];
-	$enddate = "2012-03-03";
+	//$enddate = "2012-03-01";
 
-	$start = date_create_from_format("d-M-Y", $startdate);
-	$end = date_create_from_format("d-M-Y", $enddate);
+	//$start = date_create_from_format("d-M-Y", $startdate);
+	//$end = date_create_from_format("d-M-Y", $enddate);
 	
-	$diff = abs(strtotime($end) - strtotime($start));
+	$diff = abs(strtotime($enddate) - strtotime($startdate));
 	if ($diff < 0) {
 		//ERROR: END DATE IS BEFORE START DATE
 	} else {
 		$hours = floor ($diff/$SECONDS_PER_HOUR);
 		if ($hours > $HOURLY_VIEW_MAX && $period == "Hourly") {
 			$period = "Daily";
+			$message = "Too much data to display hourly, switching to daily view";
 		}
-		$days = floor($diff / ($SECONDS_PER_DAY));
+		$days = floor($hours / ($HOURS_PER_DAY));
 		if ($days > $DAILY_VIEW_MAX && $period == "Daily") {
 			$period = "Weekly";
+			$message = "Too much data for hourly view, switching to weekly view";
 		} 
-		$weeks = ceiling($days / $DAYS_PER_WEEK);
+		$weeks = ceil($days / $DAYS_PER_WEEK);
 		if ($weeks > $WEEKLY_VIEW_MAX && $period == "Weekly") {
 			$period = "Monthly";
+			$message = "Too much data for weekly view, switching to monthly view";
 		} 
-		$months = ceiling($weeks / $WEEKS_PER_MONTH);
+		$months = ceil($weeks / $WEEKS_PER_MONTH);
 		if ($weeks > $MONTHLY_VIEW_MAX && $period == "Monthly") {
 			$period = "Yearly";
+			$message = "Too much data for monthly view, switching to yearly view";
 		}
 	}
-	//TODO: Pull data between startdate and enddate
-//} else if (ISSET($_GET['date'])) {	
-} else if (ISSET($date)) {
-	//$date = $_GET['date'];
-	$date = "2012-03-01";
 
-	//$jsonResults = '"data": {';
+	$bigArray =array(); 
 
-	$bigArray = array();
 	foreach ($apartments as $apartment) {
-	//	$jsonResults += '"'+$apartment+'": {';
-		foreach ($sensors as $sensor) {
-			//$jsonResults += '"'+$sensor+'": [';
-			$data = Engineer::db_pull_query($apartment, $sensor, $date, $period);
-			foreach ($data as $d) {
-				//$jsonResults += '"'+$d+'", ';
-				$bigArray[$apartment][$sensor] = $d;
+        //      $jsonResults += '"'+$apartment+'": {';
+                foreach ($sensors as $sensor) {
+                        //$jsonResults += '"'+$sensor+'": [';
+			//echo var_dump($data);
+                        $data = Engineer::db_pull_query($apartment, $sensor, $startdate, $enddate, $period);
+			//echo var_dump($data);
+			foreach ($data as $date=>$d) {
+				//$date = $d['Date'];
+				//echo var_dump($date);
+				//echo var_dump($d);
+				$bigArray[$apartment][$date][$sensor] = $d[$sensor];
 			}
-			//$jsonResults += '],';
-		}	
-		//$jsonResults += '],';
+                        //$jsonResults += '],';
+                }       
+                //$jsonResults += '],';
 
-	}
-
-//	$jsonResults += '}';
-	if (ISSET($startdate) && ($enddate)) {
-		//$jsonResults += ', "from": "'+$startdate+'", "to": "'+$enddate+'",';
-	} else if (ISSET($date)) {
-		//$jsonResults += ', "date": "'+$date+'",';
-	}
-	//$jsonResults += '"granularity" : "'+$period+'"';
-//	echo vardump($bigArray);
-}
-
+        }
+	$bigArray['granularity'] = $period;
+	$bigArray['message'] = $message;
+        $json = json_encode($bigArray);
+        echo $json;
+//} 
