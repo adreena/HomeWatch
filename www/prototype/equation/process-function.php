64e8551502244bc54e7d7758941349f22f0b3395
maschmey <maschmey@ualberta.ca>
2013-03-23 22:58:30 -0600

    Initial prototype test page for equation configuration.

---
+++ www/prototype/equation/process-function.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use \UASmartHome\EquationParser;
+
+$context = array();
+$context["startdate"] = "2012-02-29:0";
+$context["enddate"] = "2012-03-01:0";
+$context["apartment"] = 1;
+$context["granularity"] = "Daily";
+$context["function"] = "9 * (3+pi) * \$air_temperature$ + \$air_co2$ / 4";       //$_GET['function'];
+$context["functionname"] = "functionname";
+
+json_encode($context);
+
+$result = EquationParser::getData($context);
+
+echo "RESULT: " . $result;
+
