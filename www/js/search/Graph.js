f6dd2a699a75998eea36a06a305dc0bb10cff0e4
Eddie Antonio Santos <easantos@ualberta.ca>
2013-03-25 18:28:01 -0600

    Added skeleton of the Graph class.

---
+++ www/js/search/Graph.js
@@ -0,0 +1,40 @@
+/*
+ * Graph class skeleton.
+ */
+define("search/Graph",
+    ['jquery',
+    'underscore'
+    // TODO: add all of the Flot includes here!
+    ],
+function ($, _) {
+
+    /**
+     * Constructor for a graph.
+     */
+    function Graph(element, _clickCallback, initialData) {
+        // initialData will have .graphType appended to it.
+        this.clickCallback = _clickCallback;
+        this.element = element;
+
+        this.graphType = initialData.graphType;
+
+        this.update(initialData);
+    }
+
+
+
+    /** Update method. Provide new data to update the graph. */
+    Graph.prototype.update = function (newData) {
+        // Actually graphs the data!
+        
+    };
+
+    Graph.prototype.method = function (vars) {
+
+    };
+
+
+    /* This module exports one public member -- the class itself. */
+    return Graph;
+
+});
