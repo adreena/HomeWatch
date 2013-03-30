3682bba6646d7821e2440eb37faa3060c7beb92d
tsungyong <tsunglin@ualberta.ca>
2013-03-30 16:24:38 -0600

    Removed debug statements in Connection2

--- www/lib/UASmartHome/Database/Connection2.php
+++ www/lib/UASmartHome/Database/Connection2.php
@@ -8,9 +8,7 @@ class Connection2 {
 	 try {
 	 	$conn= new \PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME2, DB_USER, DB_PASS);
 	    $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-		$a="Connected Successfully";
 		set_time_limit(0);
-		echo $a ;
 	     }
 	 catch (\PDOException $e) {
        die('Failed to Connect' . $e->getMessage());
