a8f6c87ca83bb6dedf338cfb677cc2b8b3b9cf65
Eddie Antonio Santos <easantos@ualberta.ca>
2013-03-30 19:59:48 -0600

    Tweaked twig templates again.

--- www/search/index.php
+++ www/search/index.php
@@ -12,7 +12,7 @@ use \UASmartHome\EquationParser;
 use \UASmartHome\Database\Configuration\ConfigurationDB;
 
 //TODO: UNCOMMENT THE FOLLOWING LINE:
-//Firewall::instance()->restrictAccess(Firewall::ROLE_ENGINEER, Firewall::ROLE_MANAGER);
+Firewall::instance()->restrictAccess(Firewall::ROLE_ENGINEER, Firewall::ROLE_MANAGER);
 
 $categories = array();
 
@@ -25,23 +25,42 @@ foreach ($unfilteredCategories['functions'] as $func)  {
     $categories['Formulae'][$func['id']] = $func['name'];
 };
 
-$categories['Alerts'] = array();
-foreach ($unfilteredCategories['alerts'] as $alert)  {
-    /* That's right. Name => Name. */
-    $categories['Alerts'][$alert['name']] = $alert['name'];
-};
+//$categories['Alerts'] = array();
+//foreach ($unfilteredCategories['alerts'] as $alert)  {
+//    $categories['Alerts'][$alert['name']] = $alert['name'];
+//};
 
 $categories['Sensors'] = array();
 foreach (EquationParser::$DBVARS as $grossName => $DBName) {
-    $categories['Sensors'][$DBName] = $grossName;
+    /* That's right. Name => Name. */
+    $categories['Sensors'][$grossName] = $grossName;
 };
 
 // TODO: Still need apartment data!
+
+
+/* Get session user ID. */
+switch (User::getSessionUser()->getRoleID()) {
+    case User::ROLE_DEV:
+    case User::ROLE_ADMIN:
+    case User::ROLE_MANAGER:
+        $role = 'manager';
+        break;
+    case User::ROLE_ENGINEER:
+        $role = 'engineer';
+        break;
+    default:
+        $homepage = 'resident/home.html';
+        die();
+};
+
 
 $twig = \UASmartHome\TwigSingleton::getInstance();
 
 echo $twig->render("manager/search.html", array(
     "categories" => json_encode($categories),
-    "apartments" => json_encode(array(1,2,3,4,5,6))
+    // TODO: un-hardcode this
+    "apartments" => json_encode(array(1,2,3,4,5,6)),
+    "userRole" => $role
 ));
 
