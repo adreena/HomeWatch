36701e05c0bfb9b8824cee38658fc968452473f9
Ahmed Ghoneim <aghoneim@ualberta.ca>
2013-03-31 20:29:05 -0600

    Added GetPrices

--- www/lib/UASmartHome/Database/Utilities/UtilitiesDB.php
+++ www/lib/UASmartHome/Database/Utilities/UtilitiesDB.php
@@ -98,6 +98,21 @@ class UtilitiesDB {
 
         return true;
     }
+	//The Dates in the cloumn are Time Stamp now which means you need to pass it like that "0000-00-00"
+		public 	function Utilities_getPrice ($Type,$StartDate,$EndDate)
+	{
+			   $conn=new Connection ();
+		      $Query=$conn->connect()->prepare("select price from Utilities_Prices  
+		        where Start_Date between  :SD AND :ED and Type= :TP") ;
+			$Query->bindValue(":SD",$StartDate);
+			$Query->bindValue(":ED",$EndDate);
+			$Query->bindValue(":TP",$Type);
+			$Query->execute();
+			$row = $Query->fetch(\PDO::FETCH_ASSOC);
+			 $result=(array)$row;
+			 return $result;
+	
+	}
 
 }
 
