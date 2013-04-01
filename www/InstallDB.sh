14efe2d67f05088b77e35d507afaf8379a7da33a
aghoneim0 <aghoneim@ualberta.ca>
2013-04-01 09:45:35 -0600

    Update InstallDB.sh

--- www/InstallDB.sh
+++ www/InstallDB.sh
@@ -4,8 +4,8 @@
 
 user=root
 password=n342m8wu9
-DB1=test
-DB2=test2
+DB=TEST1
+DB1=TEST2
 ########################################################################
 #Install the Login tables   
 #########################################################################
@@ -25,43 +25,47 @@ DB2=test2
 #Each Table should get 4 Views  
 #########################################################################
 
-# A) Install the View for Air 
-echo "Installing the Views for Air"
-mysql -u $user -p$password $DB<WV.sql
+# A) Install the V0 views for all tables 
+echo "Installing the V0 Views for all tables (1 of 7)"
+mysql -u $user -p$password $DB<v0_views.sql
 
-# B)Install the view for Water 
-echo "Installing the Views for Water"
-mysql -u $user -p$password $DB<WV.sql
+# B) Install the View for Air 
+echo "Installing the Views for Air (2 of 7)"
+mysql -u $user -p$password $DB<Air_views.sql
 
-# C) Install the View for Heating 
-echo "Installing the Views for Heating"
-mysql -u $user -p$password $DB<WV.sql
+# C)Install the view for Water 
+echo "Installing the Views for Water (3 of 7)"
+mysql -u $user -p$password $DB<Water_views.sql
 
-# D) Install the View for Heat Flux
-echo "Installing the Views for Heat Flux"
-mysql -u $user -p$password $DB<WV.sql
+# D) Install the View for Heating 
+echo "Installing the Views for Heating (4 of 7)"
+mysql -u $user -p$password $DB<Heating_views.sql
 
-# E) Install the View for El Energy 
-echo "Installing the Views for El Energy"
-mysql -u $user -p$password $DB<WV.sql
+# E) Install the View for Heat Flux
+echo "Installing the Views for Heat Flux (5 of 7)"
+mysql -u $user -p$password $DB<Heatflux_views.sql
 
-# F) Install the Weather Forecast Pre-Historical Data
-echo "Installing the Table Weather Forecast with Pre-Historical Data"
+# F) Install the View for El Energy 
+echo "Installing the Views for El Energy (6 of 7)"
+mysql -u $user -p$password $DB<Energy_views.sql
+
+# G) Install the Weather Forecast Pre-Historical Data
+echo "Installing the Table Weather Forecast with Pre-Historical Data (7 of 7)"
 mysql -u $user -p$password $DB<WV.sql
 #################################################################
 #Install the Tables for The  Required for the 
 #################################################################
 
 # A) Install Tables flow_minute ,temp_minute,boiler_loop_minute,Anaylze,COPCal
-echo "Installing the Bas Tables for Flow minute ,Temp Minute ,Boiler minute,Anaylze and CopCa"
-mysql -u $user -p$password $DB1<WV.sql
+echo "Installing the Bas Tables for Flow minute ,Temp Minute ,Boiler minute,Anaylze and CopCa (1 of 3)"
+mysql -u $user -p$password $DB1<BasTables.sql
  
 # B) Install the Views V0_energy and Energy_Minute
-echo "Installing the Views for Energy and Eenergy_Minute"
-mysql -u $user -p$password $DB1<WV.sq
+echo "Installing the Views for Energy and Eenergy_Minute (1 of 3)"
+mysql -u $user -p$password $DB1<BasViews.sq
 
 # C) Install the MySQL Functions  calc_cop_test,calc_cop,EQ2_Part 1 
 #and EQ2_Part2
-echo "Installing the MYSQL Functions "
-mysql -u $user -p$password $DB<WV.sql
+echo "Installing the MYSQL Functions (3 of 3)"
+mysql -u $user -p$password $DB<Bascalculations.sql
 ##################################################################
