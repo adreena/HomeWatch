85a9fc0f48a4fd4e2b00134aa885292458ac4210
aghoneim0 <aghoneim@ualberta.ca>
2013-04-01 09:48:12 -0600

    Update InstallDB.sh

--- www/InstallDB.sh
+++ www/InstallDB.sh
@@ -62,7 +62,7 @@ mysql -u $user -p$password $DB1<BasTables.sql
  
 # B) Install the Views V0_energy and Energy_Minute
 echo "Installing the Views for Energy and Eenergy_Minute (1 of 3)"
-mysql -u $user -p$password $DB1<BasViews.sq
+mysql -u $user -p$password $DB1<BasViews.sql
 
 # C) Install the MySQL Functions  calc_cop_test,calc_cop,EQ2_Part 1 
 #and EQ2_Part2
