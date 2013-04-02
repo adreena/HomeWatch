297c66a4c99340e7e070204253d406928f89c5b4
aghoneim0 <aghoneim@ualberta.ca>
2013-04-02 00:27:06 -0600

    Update Engineer2.php

--- www/lib/UASmartHome/Database/Engineer2.php
+++ www/lib/UASmartHome/Database/Engineer2.php
@@ -84,11 +84,11 @@ class Engineer2 {
 	}
 }
 
-    public static function getEnergyColumnData($datefrom, $dateto, $granularity, $column) {
+    public static function getEnergyColumnData($datefrom, $dateto,$column,$granularity=null) {
         /* Assume that all dates are in a "canonical" format -- that is, 
         * prevent the server's default timezone from affecting the date. */
         date_default_timezone_set('UTC');
-        
+       /* 
         $intervalString = null;
         switch ($granularity) {
             case "Hourly": $intervalString = '1 hour'; break;
@@ -97,15 +97,15 @@ class Engineer2 {
             case "Monthly":
             default: $intervalString = '1 month'; break;
         }
-        
+        *//*
         $interval = \DateInterval::createFromDateString($intervalString);
         $period = new \DatePeriod($datefrom, $interval, $dateto);
 
          $strDisplayTick = $tick->format("Y-m-d:G");     
           $strTick = $tick->format("Y-m-d G"); 
-         $strTickEnd = $tick->add($interval)->format("Y-m-d G");
+         $strTickEnd = $tick->add($interval)->format("Y-m-d G");*/
         $data = null;
-        $data=self::EQ($strTick, $strTickEnd, self::ENERGY_EQ, $column);
+        $data=self::EQ($datefrom, $dateto, self::ENERGY_EQ, $column);
       /*  foreach ($period as $tick) {
             $strDisplayTick = $tick->format("Y-m-d:G");
             
