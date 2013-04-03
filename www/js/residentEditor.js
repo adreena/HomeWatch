c5376e82e6b72064b551fda04971644cf10ac49f
maschmey <maschmey@ualberta.ca>
2013-04-02 22:57:06 -0600

    Resident editor now uses require js and has slightly improved validation

--- www/js/residentEditor.js
+++ www/js/residentEditor.js
@@ -1,126 +1,149 @@
+require(['jquery',
+         'vendor/jquery-ui',
+         'vendor/jquery.scrollTo-min',
+         'vendor/jquery.validate.min'],
 
-// TODO: use require js
-// Requires:
-// - jquery-ui
-// - jquery.scrollto
+function ($) {
+    "use strict";
 
-var SCROLL_SPEED = 200;
-var SCROLL_OPTIONS = {offset: -100}
+    var SCROLL_SPEED = 200;
+    var SCROLL_OPTIONS = {offset: -100}
 
-var residentEditorID = "#resident-editor";
+    var RESIDENT_EDITOR_ID = "#resident-editor";
+    var RESIDENT_DISPLAY_ID = "#resident-display";
 
-var residentEditor;
-
-$(window).load(function() {
-
-    initResidentEditor();
-});
-
-function initResidentEditor() {
-    residentEditor = $(residentEditorID)[0];
-    residentEditor.reset();
-
-    $(residentEditor).validate({
-        submitHandler: updateResident,
-        rules: {
-	        name: "required",
-	        username: "required",
-	        room: "requred"
-	    },
-	    errorElement: "div",
-        errorPlacement: function(error, element) {
-            $(element).prev().before(error);
-        },
-	    onkeyup: false
+    var residentEditor;
+    
+    $(function () {
+        initResidentEditor();
+        
+        // Init editor reset buttons
+        $(document).find('.button.editor-reset').click(resetEditor);
     });
-}
 
-// =================================================================================================
-// RESIDENT CONFIG
-// =================================================================================================
-function editResident(editButton) {
-    var residentData = getRowData(editButton);
-    setResidentEditorData(residentData);
-    editConfig(residentEditor, residentData);
-}
-
-function updateResident() {
-    $.post('/manager/update-resident.php', getResidentEditorData())
-    .done(function(data) {
+    // =============================================================================================
+    // RESIDENT CONFIG
+    // =============================================================================================
+    function initResidentEditor() {
+        // Init the edit and delete buttons
+        //$(RESIDENT_DISPLAY_ID).find('.delete-resident').click(deleteResident);
+        $(RESIDENT_DISPLAY_ID).find('.edit-resident').click(editResident);
+        
+        // Init the editor form
+        residentEditor = $(RESIDENT_EDITOR_ID)[0];
         residentEditor.reset();
-        location.reload();
-    })
-    .fail(function(data) {
-        alert("Error Submitting Resident: " + data.statusText);
-    });
+        
+        $(residentEditor).validate({
+            submitHandler: updateResident,
+            rules: {
+	            name: "required",
+	            username: "required",
+	            room: "required"
+	        },
+	        errorElement: "div",
+            errorPlacement: function(error, element) {
+                $(element).prev().before(error);
+            },
+	        onkeyup: false
+        });
+        
+        setEditorEnabled(residentEditor, false);
+    }
+    
+    function editResident(event) {
+        var editButton = event.target;
+        var residentData = getRowData(editButton);
+        setResidentEditorData(residentData);
+        editConfig(residentEditor, residentData);
+        
+        setEditorEnabled(residentEditor, true);
+    }
 
-    return false;
-}
+    function updateResident() {
+        $.post('/manager/update-resident.php', getResidentEditorData())
+        .done(function(data) {
+            residentEditor.reset();
+            location.reload();
+        })
+        .fail(function(data) {
+            alert("Error Updating Resident: " + data.statusText);
+        });
 
-function deleteResident(deleteButton) {
-    var residentID = getRowData(deleteButton).id;
+        return false;
+    }
 
-    $.post('/manager/delete-resident.php', {id: residentID})
-    .done(function(data) { window.location.reload(); })
-    .fail(function(data) { alert("Error deleting resident: " + data.statusText); });
-}
+    function deleteResident(event) {
+        var deleteButton = event.target;
+        var residentID = getRowData(deleteButton).id;
 
-function getResidentEditorData() {
-    var residentEditorContents = $(residentEditor).contents();
+        $.post('/manager/delete-resident.php', {id: residentID})
+        .done(function(data) { window.location.reload(); })
+        .fail(function(data) { alert("Error deleting resident: " + data.statusText); });
+    }
 
-    return {
-        id: residentEditorContents.find('input[name=id]').val(),
-        name: residentEditorContents.find('input[name=name]').val(),
-        username: residentEditorContents.find('input[name=username]').val(),
-        room: residentEditorContents.find('input[name=room]').val(),
-        location: residentEditorContents.find('input[name=location]').val(),
-        roomstatus: residentEditorContents.find('input[name=roomstatus]').val()
-    };
-}
+    function getResidentEditorData() {
+        var residentEditorContents = $(residentEditor).contents();
 
-function setResidentEditorData(resident) {
-    var residentEditorContents = $(residentEditor).contents();
-    residentEditorContents.find('input[name=name]').val(resident.name);
-    residentEditorContents.find('input[name=username]').val(resident.username);
-    residentEditorContents.find('input[name=room]').val(resident.room);
-    residentEditorContents.find('input[name=location]').val(resident.location);
-    residentEditorContents.find('input[name=roomstatus]').val(resident.roomstatus);
-    residentEditorContents.find('input[name=id]').val(resident.id);
-}
+        return {
+            id: residentEditorContents.find('input[name=id]').val(),
+            name: residentEditorContents.find('input[name=name]').val(),
+            username: residentEditorContents.find('input[name=username]').val(),
+            room: residentEditorContents.find('input[name=room]').val(),
+            location: residentEditorContents.find('input[name=location]').val(),
+            roomstatus: residentEditorContents.find('input[name=roomstatus]').val()
+        };
+    }
 
-// =================================================================================================
-// GENERAL CONFIG
-// =================================================================================================
-function getRowData(rowButton) {
-    var row = $(rowButton).closest("tr");
+    function setResidentEditorData(resident) {
+        var residentEditorContents = $(residentEditor).contents();
+        residentEditorContents.find('input[name=name]').val(resident.name);
+        residentEditorContents.find('input[name=username]').val(resident.username);
+        residentEditorContents.find('input[name=room]').val(resident.room);
+        residentEditorContents.find('input[name=location]').val(resident.location);
+        residentEditorContents.find('input[name=roomstatus]').val(resident.roomstatus);
+        residentEditorContents.find('input[name=id]').val(resident.id);
+    }
 
-    return {
-        id: row.attr('id').match(/\d+/)[0],
-        name: $(row.children(".name")[0]).text(),
-        username: $(row.children(".username")).text(),
-        room: $(row.children(".room")).text(),
-        location: $(row.children(".location")).text(),
-        roomstatus: $(row.children(".roomstatus")).text()
-    };
-}
+    // =============================================================================================
+    // GENERAL CONFIG
+    // =============================================================================================
+    function getRowData(rowButton) {
+        var row = $(rowButton).closest("tr");
 
-function editConfig(editor, data) {
-    $.scrollTo(editor, SCROLL_SPEED, SCROLL_OPTIONS);
+        return {
+            id: row.attr('id').match(/\d+/)[0],
+            name: $(row.children(".name")[0]).text(),
+            username: $(row.children(".username")).text(),
+            room: $(row.children(".room")).text(),
+            location: $(row.children(".location")).text(),
+            roomstatus: $(row.children(".roomstatus")).text()
+        };
+    }
 
-    // Make the legend red and add (EDITING "NAME") to text
-    var legend = $(editor).find("legend")[0];
-    legend.style.color = 'red';
-    legend.innerHTML = legend.innerHTML.replace(/\(.*$/, ""); // Remove any existing (EDITING "NAME") text
-    legend.innerHTML += " (EDITING \"" + data.name + "\")";
-}
+    function editConfig(editor, data) {
+        $.scrollTo(editor, SCROLL_SPEED, SCROLL_OPTIONS);
 
-function resetEditor(clearButton) {
-    var form = $(clearButton).closest("form")[0];
-    form.reset();
+        // Make the legend red and add (EDITING "NAME") to text
+        var legend = $(editor).find("legend")[0];
+        legend.style.color = 'red';
+        legend.innerHTML = legend.innerHTML.replace(/\(.*$/, ""); // Remove any existing (EDITING "NAME") text
+        legend.innerHTML += " (EDITING \"" + data.name + "\")";
+    }
 
-    var legend = $(form).find("legend")[0];
-    legend.style.color = 'black';
+    function resetEditor(event) {
+        var clearButton = event.target;
+        var form = $(clearButton).closest("form")[0];
+        form.reset();
 
-    legend.innerHTML = legend.innerHTML.replace(/\(.*$/, "");
-}
+        var legend = $(form).find("legend")[0];
+        legend.style.color = 'black';
 
+        legend.innerHTML = legend.innerHTML.replace(/\(.*$/, "");
+        
+        setEditorEnabled(residentEditor, false);
+    }
+    
+    function setEditorEnabled(editor, enabled) {
+        $(editor).find('input,button').attr('disabled', !enabled);;
+    }
+});
