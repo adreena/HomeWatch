937754b8af4ef76ecabb681886783683d8245c10
aghoneim0 <aghoneim@ualberta.ca>
2013-04-25 16:58:00 -0600

    Update config.php

--- scripts/config.php
+++ scripts/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('DB_TYPE') ? NULL : define('DB_TYPE', 'mysql');
-defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
-defined('DB_USER') ? NULL : define('DB_USER', 'root');
-defined('DB_PASS') ? NULL : define('DB_PASS', 'n342m8wu9');
-defined('DB_NAME') ? NULL : define('DB_NAME', 'Smart_Condo');
+defined('DB_HOST') ? NULL : define('DB_HOST', 'hypatia.cs.ualberta.ca');
+defined('DB_USER') ? NULL : define('DB_USER', 'aghoneim');
+defined('DB_PASS') ? NULL : define('DB_PASS', 'usy7f5zW');
+defined('DB_NAME') ? NULL : define('DB_NAME', 'smarthome_apts');
 ?>
