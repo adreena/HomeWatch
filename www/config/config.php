663891e53edf97565e8479c09e9e9adfd5299369
aghoneim0 <aghoneim@ualberta.ca>
2013-04-25 16:34:38 -0600

    Update config.php

--- www/config/config.php
+++ www/config/config.php
@@ -13,11 +13,11 @@
 /** The following are defines to setup the database.
  * The defaults here are set to the one on the DB developer's machine. */
 defined('DB_TYPE') ? NULL : define('DB_TYPE', 'mysql');
-defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
-defined('DB_USER') ? NULL : define('DB_USER', 'root');
-defined('DB_PASS') ? NULL : define('DB_PASS', 'n342m8wu9');
-defined('DB_NAME') ? NULL : define('DB_NAME', 'Smart_Condo');
-defined('DB_NAME2') ? NULL : define('DB_NAME2', 'Smart_Condo_Bas');
+defined('DB_HOST') ? NULL : define('DB_HOST', 'hypatia.cs.ualberta.ca');
+defined('DB_USER') ? NULL : define('DB_USER', 'aghoneim');
+defined('DB_PASS') ? NULL : define('DB_PASS', 'usy7f5zW');
+defined('DB_NAME') ? NULL : define('DB_NAME', 'smarthome_apts');
+defined('DB_NAME2') ? NULL : define('DB_NAME2', 'smarthome_bas');
 
 /**
  * This is the absolute path to the root directory of the web project.
