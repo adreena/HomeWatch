f3788a7cba3157a9ff484f2454fab5ce246ad52d
vganev <vganev@ualberta.ca>
2013-04-27 07:42:52 -0600

    fixed DB address

--- www/config/config.php
+++ www/config/config.php
@@ -22,7 +22,7 @@ defined('DB_NAME2') ? NULL : define('DB_NAME2', 'Smart_Bas');
 */
 //New DB
 defined('DB_TYPE') ? NULL : define('DB_TYPE', 'mysql');
-defined('DB_HOST') ? NULL : define('DB_HOST', '10.0.2.2');
+defined('DB_HOST') ? NULL : define('DB_HOST', 'hypatia.cs.ualberta.ca');
 defined('DB_USER') ? NULL : define('DB_USER', 'aghoneim');
 defined('DB_PASS') ? NULL : define('DB_PASS', 'usy7f5zW');
 defined('DB_NAME') ? NULL : define('DB_NAME', 'smarthome_apts');
